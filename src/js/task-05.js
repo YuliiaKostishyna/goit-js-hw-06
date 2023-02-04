5c6f1459ba3d88086520f7f8adfdcea68f5f5461
YuliiaKostishyna <112808612+YuliiaKostishyna@users.noreply.github.com>
2023-02-04 17:07:14 +0200

    123

--- src/js/task-05.js
+++
@@ -1,9 +0,0 @@
-const textInput = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
-
-textInput.addEventListener("input", (event) => {
-    if (textInput.value === "") {
-        return output.textContent = "Anonymous";
-    }
-  return output.textContent = event.currentTarget.value;
-});
--- src/js/task-07.js
+++
@@ -1,6 +0,0 @@
-const textInput = document.querySelector("#font-size-control");
-const text = document.querySelector("#text");
-textInput.addEventListener("input", (event) => {
-    text.style.fontSize = `${event.currentTarget.value}px` ;
-}
-);
